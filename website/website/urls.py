7ebcf1871209316607ff983d451760f6e4970440
Ivann <ivann.galic@edu.itescia.fr>
2020-06-17 23:29:43 +0200

    Récupération url file ok

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from noesoft import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('annonce_1/', views.annonce_1, name='annonce_1'),
     path('annonce_1_confirmed/<str:fname>/', views.annonce_1_confirmed, name='annonce_1_confirmed')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
